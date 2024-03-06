3a1f81c99fc621bde3fbf558a202f2d3ab765a8f
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-06 15:30:56 +0300

    Created a struct for Marios x & y positions as well as a Mario struct

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+struct Position{
+    x: u32,
+    y: u32,
+}
+
+
+struct Marion{
+    position: Position,
+    coins: u32,
+    power_up: Option<PowerUp>
+}
+
+fn main() {
+    println!("Hello, world!");
+}
